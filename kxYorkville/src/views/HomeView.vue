10bfe0b8d989642da24112ed199bc1451263577e
TLNRB <nortol01@easv365.dk>
2023-08-30 12:00:34 +0200

    button component separated

--- kxYorkville/src/views/HomeView.vue
+++ kxYorkville/src/views/HomeView.vue
@@ -3,6 +3,11 @@ import { ref, onMounted, onUnmounted } from 'vue'
 import videoDesktop from '../assets/images/weights.mp4'
 import videoMobile from '../assets/images/weightsVertical.mp4'
 
+import Button from '../components/UI/Button.vue'
+
+//Button content
+const buttonContent = ['Classes', 'Timetable']
+
 /* ----- Checking if the screen size is mobile ----- */
 const isMobile = ref(false)
 
@@ -40,7 +45,9 @@ onUnmounted(() => {
       <div
         class="flex flex-wrap justify-center gap-[1.5rem] mt-[2.5rem] xs:gap-[2rem] lg:mt-[3rem]"
       >
-        <button class="flex flex-col w-fit text-[1.25rem] relative group">
+        <Button :content="buttonContent[0]" />
+        <Button :content="buttonContent[1]" />
+        <!-- <button class="flex flex-col w-fit text-[1.25rem] relative group">
           <span
             class="font-[600] py-[.375rem] px-[1.125rem] border-[1px] bg-primaryColor text-textLight border-primaryColor z-[1] group-hover:bg-transparent group-hover:border-primaryColor ease-in duration-[.15s] delay-[.05s] md:py-[.5rem] md:px-[1.25rem] md:text-[1.5rem]"
             >Classes</span
@@ -59,7 +66,7 @@ onUnmounted(() => {
             class="font-[600] py-[.375rem] px-[1.125rem] text-transparent border-[1px] border-textLight absolute top-[4px] right-[-4px] group-hover:top-[0] group-hover:right-[0] ease-in duration-[.2s] md:py-[.5rem] md:px-[1.25rem] md:text-[1.5rem]"
             >Timetable</span
           >
-        </button>
+        </button> -->
       </div>
     </div>
   </section>
