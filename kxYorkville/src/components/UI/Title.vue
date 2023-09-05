046e51d8ae78bc62b5d5c4a4676e940d14eb064d
TLNRB <nortol01@easv365.dk>
2023-09-05 11:07:21 +0200

    title component created

---
+++ kxYorkville/src/components/UI/Title.vue
@@ -0,0 +1,11 @@
+<script setup>
+//Prop handling
+const { content } = defineProps(['content'])
+</script>
+
+<template>
+  <div class="flex flex-col-reverse items-center gap-[0rem] md:flex-row md:gap-[1rem]">
+    <hr class="w-[2.5rem] border-[2px] translate-y-[4px] border-primaryColor" />
+    <h2 class="text-[3rem] font-semibold sm:text-[4rem]">{{ content }}</h2>
+  </div>
+</template>
--- kxYorkville/src/views/HomeView.vue
+++ kxYorkville/src/views/HomeView.vue
@@ -4,6 +4,7 @@ import { ref, onMounted, onUnmounted } from 'vue'
 import Hero from '../components/HomePage/Hero.vue'
 import Classes from '../components/HomePage/Classes.vue'
 import About from '../components/HomePage/About.vue'
+import Title from '../components/UI/Title.vue'
 /* ----- Import assets ----- */
 import videoDesktop from '../assets/images/weights.mp4'
 import videoMobile from '../assets/images/weightsVertical.mp4'
@@ -31,6 +32,10 @@ onUnmounted(() => {
   <Hero :videoDesktop="videoDesktop" :videoMobile="videoMobile" :isMobile="isMobile" />
   <Classes />
   <About :studio="studio" />
+  <section class="py-[4rem] px-[1rem] md:py-[6rem] md:px-[2rem]">
+    <Title content="coaches" />
+    <div></div>
+  </section>
 </template>
 
 <style scoped></style>
