1cef1acc0504090b7719a0c1c492b454ccfcde72
TLNRB <nortol01@easv365.dk>
2023-09-08 12:40:52 +0200

    grid class names added through a loop

--- kxYorkville/src/components/HomePage/Info.vue
+++ kxYorkville/src/components/HomePage/Info.vue
@@ -1,14 +1,23 @@
-<script setup></script>
+<script setup>
+const gridClassNameArray = [
+  'grid-one',
+  'grid-two',
+  'grid-three',
+  'grid-four',
+  'grid-five',
+  'grid-six',
+  'grid-seven'
+]
+</script>
 
 <template>
   <section class="grid-container py-[4rem] xxxxl:py-[6rem]">
-    <div class="one bg-primaryColor border-[1px] border-black"></div>
-    <div class="two bg-primaryColor border-[1px] border-black"></div>
-    <div class="three bg-primaryColor border-[1px] border-black"></div>
-    <div class="four bg-primaryColor border-[1px] border-black"></div>
-    <div class="five bg-primaryColor border-[1px] border-black"></div>
-    <div class="six bg-primaryColor border-[1px] border-black"></div>
-    <div class="seven bg-primaryColor border-[1px] border-black"></div>
+    <div
+      v-for="gridClassName in gridClassNameArray"
+      :key="gridClassName"
+      class="bg-primaryColor border-[1px] border-black"
+      :class="gridClassName"
+    ></div>
   </section>
 </template>
 
@@ -25,37 +34,37 @@
     grid-template-columns: repeat(2, minmax(250px, 1fr));
   }
 
-  .one {
+  .grid-one {
     grid-column: 1 / 2;
     grid-row: 1 / 2;
   }
 
-  .two {
+  .grid-two {
     grid-column: 2 / 3;
     grid-row: 1 / 2;
   }
 
-  .three {
+  .grid-three {
     grid-column: 1 / 2;
     grid-row: 2 / 3;
   }
 
-  .four {
+  .grid-four {
     grid-column: 1 / 2;
     grid-row: 3 / 4;
   }
 
-  .five {
+  .grid-five {
     grid-column: 2 / 3;
     grid-row: 2 / 4;
   }
 
-  .six {
+  .grid-six {
     grid-column: 1 / 2;
     grid-row: 4 / 5;
   }
 
-  .seven {
+  .grid-seven {
     grid-column: 2 / 3;
     grid-row: 4 / 5;
   }
@@ -66,37 +75,22 @@
     grid-template-columns: repeat(3, minmax(250px, 1fr));
   }
 
-  .one {
-    grid-column: 1 / 2;
-    grid-row: 1 / 2;
-  }
-
-  .two {
-    grid-column: 2 / 3;
-    grid-row: 1 / 2;
-  }
-
-  .three {
-    grid-column: 1 / 2;
-    grid-row: 2 / 3;
-  }
-
-  .four {
+  .grid-four {
     grid-column: 2 / 3;
     grid-row: 2 / 3;
   }
 
-  .five {
+  .grid-five {
     grid-column: 3 / 4;
     grid-row: 1 / 4;
   }
 
-  .six {
+  .grid-six {
     grid-column: 1 / 2;
     grid-row: 3 / 4;
   }
 
-  .seven {
+  .grid-seven {
     grid-column: 2 / 3;
     grid-row: 3 / 4;
   }
@@ -107,37 +101,27 @@
     grid-template-columns: repeat(4, minmax(250px, 1fr));
   }
 
-  .one {
-    grid-column: 1 / 2;
-    grid-row: 1 / 2;
-  }
-
-  .two {
-    grid-column: 2 / 3;
-    grid-row: 1 / 2;
-  }
-
-  .three {
+  .grid-three {
     grid-column: 3 / 4;
     grid-row: 1 / 2;
   }
 
-  .four {
+  .grid-four {
     grid-column: 4 / 5;
     grid-row: 1 / 3;
   }
 
-  .five {
+  .grid-five {
     grid-column: 1 / 2;
     grid-row: 2 / 3;
   }
 
-  .six {
+  .grid-six {
     grid-column: 2 / 3;
     grid-row: 2 / 3;
   }
 
-  .seven {
+  .grid-seven {
     grid-column: 3 / 4;
     grid-row: 2 / 3;
   }
