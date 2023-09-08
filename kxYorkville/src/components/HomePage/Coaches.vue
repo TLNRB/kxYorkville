aabf797045d908c5285879c44e3b0aad6193e2ff
TLNRB <nortol01@easv365.dk>
2023-09-08 11:40:26 +0200

    coaches component created

---
+++ kxYorkville/src/components/HomePage/Coaches.vue
@@ -0,0 +1,37 @@
+<script setup>
+//Importing components
+import Title from '../../components/UI/Title.vue'
+//Prop handling
+const { coachesDB } = defineProps(['coachesDB'])
+</script>
+
+<template>
+  <section class="py-[4rem] px-[1rem] md:px-[2rem] xxl:py-[6rem] xxxxl:px-[4rem]">
+    <Title content="coaches" />
+    <div class="flex flex-wrap justify-center items-center gap-[2rem] my-[4rem] xxxxl:gap-[2rem]">
+      <div
+        class="relative w-[225px] h-[125px] flex flex-col justify-between py-[.75rem] px-[1rem] bg-cover bg-center-top-mid bg-no-repeat brightness-[85%] border-[1px] border-bgColorDark cursor-pointer drop-shadow-xl group xs:w-[275px] xs:h-[160px] lg:w-[300px] lg:h-[175px] xxl:w-[350px] xxl:h-[200px] xxl:py-[1rem] xxl:px-[1.25rem] xxxxl:w-[375px] xxxxl:h-[225px]"
+        :style="`background-image: url('${coaches.img}')`"
+        v-for="coaches in coachesDB"
+        :key="coaches.id"
+      >
+        <p
+          class="font-oswald text-[.875rem] text-primaryColor drop-shadow-lg xs:text-[1rem] xxl:text-[1.125rem]"
+        >
+          {{ coaches.profession }}
+        </p>
+        <h4 class="text-[1.125rem] drop-shadow-lg xxl:text-[1.25rem]">{{ coaches.name }}</h4>
+        <div
+          class="absolute left-0 top-0 w-[0px] h-[124px] py-[.75rem] flex justify-center items-center text-transparent bg-bgColorDark text-[1.25rem] group-hover:w-[100%] group-hover:text-textLight group-hover:px-[1.125rem] ease-in duration-[.2s] xs:h-[159px] lg:h-[174px] xxl:h-[199px] xxxxl:h-[224px]"
+        >
+          <font-awesome-icon
+            class="text-[2rem] xxl:text-[2.5rem]"
+            :icon="['fas', 'arrow-right-long']"
+          />
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped></style>
--- kxYorkville/src/views/HomeView.vue
+++ kxYorkville/src/views/HomeView.vue
@@ -4,7 +4,7 @@ import { ref, onMounted, onUnmounted } from 'vue'
 import Hero from '../components/HomePage/Hero.vue'
 import Classes from '../components/HomePage/Classes.vue'
 import About from '../components/HomePage/About.vue'
-import Title from '../components/UI/Title.vue'
+import Coaches from '../components/HomePage/Coaches.vue'
 /* ----- Import assets ----- */
 import videoDesktop from '../assets/images/weights.mp4'
 import videoMobile from '../assets/images/weightsVertical.mp4'
@@ -34,32 +34,7 @@ onUnmounted(() => {
   <Hero :videoDesktop="videoDesktop" :videoMobile="videoMobile" :isMobile="isMobile" />
   <Classes />
   <About :studio="studio" />
-  <section class="py-[4rem] px-[1rem] md:px-[2rem] xxl:py-[6rem] xxxxl:px-[4rem]">
-    <Title content="coaches" />
-    <div class="flex flex-wrap justify-center items-center gap-[2rem] my-[4rem] xxxxl:gap-[2rem]">
-      <div
-        class="relative w-[225px] h-[125px] flex flex-col justify-between py-[.75rem] px-[1rem] bg-cover bg-center-top-mid bg-no-repeat brightness-[85%] border-[1px] border-bgColorDark cursor-pointer drop-shadow-xl group xs:w-[275px] xs:h-[160px] lg:w-[300px] lg:h-[175px] xxl:w-[350px] xxl:h-[200px] xxl:py-[1rem] xxl:px-[1.25rem] xxxxl:w-[375px] xxxxl:h-[225px]"
-        :style="`background-image: url('${coaches.img}')`"
-        v-for="coaches in coachesDB"
-        :key="coaches.id"
-      >
-        <p
-          class="font-oswald text-[.875rem] text-primaryColor drop-shadow-lg xs:text-[1rem] xxl:text-[1.125rem]"
-        >
-          {{ coaches.profession }}
-        </p>
-        <h4 class="text-[1.125rem] drop-shadow-lg xxl:text-[1.25rem]">{{ coaches.name }}</h4>
-        <div
-          class="absolute left-0 top-0 w-[0px] h-[124px] py-[.75rem] flex justify-center items-center text-transparent bg-bgColorDark text-[1.25rem] group-hover:w-[100%] group-hover:text-textLight group-hover:px-[1.125rem] ease-in duration-[.2s] xs:h-[159px] lg:h-[174px] xxl:h-[199px] xxxxl:h-[224px]"
-        >
-          <font-awesome-icon
-            class="text-[2rem] xxl:text-[2.5rem]"
-            :icon="['fas', 'arrow-right-long']"
-          />
-        </div>
-      </div>
-    </div>
-  </section>
+  <Coaches :coachesDB="coachesDB" />
 </template>
 
 <style scoped></style>
