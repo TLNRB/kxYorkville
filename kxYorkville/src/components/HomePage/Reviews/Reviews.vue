475b907d16fe29fa966e9d20b7acf696d9432c63
TLNRB <nortol01@easv365.dk>
2023-09-10 13:58:08 +0200

    title updating

--- kxYorkville/src/components/HomePage/Reviews/Reviews.vue
+++ kxYorkville/src/components/HomePage/Reviews/Reviews.vue
@@ -1,4 +1,6 @@
 <script setup>
+import Title from '../../UI/Title.vue'
+
 //Prop handling
 const { img, imgMobile, isMobile } = defineProps(['img', 'imgMobile', 'isMobile'])
 const bgImg = `background-image: url('${img}')`
@@ -7,11 +9,13 @@ const bgImgMobile = `background-image: url('${imgMobile}')`
 
 <template>
   <section
-    class="my-[4rem] p-[20rem] bg-cover bg-center-bottom-mid bg-no-repeat brightness-[85%] xxxxl:my-[6rem]"
+    class="my-[4rem] py-[4rem] px-[1rem] bg-cover bg-center-bottom-mid bg-no-repeat brightness-[85%] md:px-[2rem] xxl:py-[6rem] xxxxl:px-[4rem] xxxxl:my-[6rem]"
     :style="!isMobile ? bgImg : bgImgMobile"
   >
-    <div class="absolute left-0 top-0 right-0 bottom-0 backdrop-blur-[10px] z-[-1]"></div>
-    <h1>hello</h1>
+    <div
+      class="absolute left-0 top-0 right-0 bottom-0 backdrop-blur-[6px] z-[-1] sm:backdrop-blur-[8px] lg:backdrop-blur-[10px]"
+    ></div>
+    <Title content="testimonials" />
   </section>
 </template>
 
--- kxYorkville/src/components/UI/Title.vue
+++ kxYorkville/src/components/UI/Title.vue
@@ -6,8 +6,12 @@ const { content } = defineProps(['content'])
 <template>
   <div class="flex flex-col-reverse items-center gap-[0rem] md:flex-row md:gap-[1rem]">
     <hr
-      class="w-[2.5rem] border-[2px] translate-y-[4px] border-primaryColor xxxxl:w-[3rem] xxxxl:translate-y-[6px]"
+      class="w-[2rem] border-[2px] translate-y-[4px] border-primaryColor xs:w-[2.5rem] xxxxl:w-[3rem] xxxxl:translate-y-[6px]"
     />
-    <h2 class="text-[3rem] font-semibold sm:text-[4rem] xxxxl:text-[6rem]">{{ content }}</h2>
+    <h2
+      class="text-[2rem] font-semibold drop-shadow-lg xs:text-[3rem] sm:text-[4rem] xxxxl:text-[6rem]"
+    >
+      {{ content }}
+    </h2>
   </div>
 </template>
