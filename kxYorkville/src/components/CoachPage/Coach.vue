ddd0ba444f28c6a6dc38f918a7b8a8c019e025ea
TLNRB <nortol01@easv365.dk>
2023-09-26 10:30:30 +0200

    coach img with name added

--- kxYorkville/src/components/CoachPage/Coach.vue
+++ kxYorkville/src/components/CoachPage/Coach.vue
@@ -6,6 +6,23 @@ import Button from '../UI/Button.vue'
 const { singleCoach } = defineProps(['singleCoach'])
 </script>
 
-<template></template>
+<template>
+  <main
+    class="flex flex-col-reverse pt-[6rem] px-[1.5rem] md:pt-[8rem] md:px-[2rem] lg:flex-row lg:px-[4rem] xxl:pt-[8rem] xxxxl:pt-[10rem]"
+  >
+    <section></section>
+    <section class="relative">
+      <img
+        :src="singleCoach.img"
+        class="w-[100%] h-[500px] brightness-[90%] drop-shadow-xl rounded-l-[15px] rounded-br-[15px] sm:h-[600px] md:h-[750px] md:rounded-l-[20px] md:rounded-br-[20px]"
+      />
+      <div
+        class="absolute top-[-.25rem] right-[-.25rem] py-[.75rem] px-[1.5rem] bg-bgDark font-[600] text-textGray md:text-[2rem] rounded-bl-[15px] md:rounded-bl-[17px]"
+      >
+        {{ singleCoach.name }}
+      </div>
+    </section>
+  </main>
+</template>
 
 <style scoped></style>
