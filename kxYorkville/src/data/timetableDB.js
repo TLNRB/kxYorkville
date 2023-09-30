c7ae5d36e2f22cffe9c0c343cf991c48d5f502e3
TLNRB <nortol01@easv365.dk>
2023-09-30 09:52:42 +0200

    timetable database created

---
+++ kxYorkville/src/data/timetableDB.js
@@ -0,0 +1,249 @@
+const timetableDB = [
+  {
+    id: 1,
+    day: 'Monday',
+    class: [
+      {
+        id: 1,
+        name: 'Yoga',
+        from: '9:00',
+        to: '9:45',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 2,
+        name: 'Crossfit',
+        from: '11:00',
+        to: '11:50',
+        coach: 'John Doe'
+      },
+      {
+        id: 3,
+        name: 'Crossfit',
+        from: '15:00',
+        to: '15:50',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 4,
+        name: 'Boxing',
+        from: '18:00',
+        to: '19:00',
+        coach: 'John Doe'
+      }
+    ]
+  },
+  {
+    id: 2,
+    day: 'Tuesday',
+    class: [
+      {
+        id: 1,
+        name: 'Yoga',
+        from: '8:30',
+        to: '9:15',
+        coach: 'John Doe'
+      },
+      {
+        id: 2,
+        name: 'Crossfit',
+        from: '12:00',
+        to: '12:50',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 3,
+        name: 'Yoga',
+        from: '17:00',
+        to: '17:45',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 4,
+        name: 'Boxing',
+        from: '19:00',
+        to: '20:00',
+        coach: 'John Doe'
+      }
+    ]
+  },
+  {
+    id: 3,
+    day: 'Wednesday',
+    class: [
+      {
+        id: 1,
+        name: 'Yoga',
+        from: '9:30',
+        to: '10:15',
+        coach: 'John Doe'
+      },
+      {
+        id: 2,
+        name: 'Crossfit',
+        from: '11:00',
+        to: '11:50',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 3,
+        name: 'Crossfit',
+        from: '17:00',
+        to: '17:50',
+        coach: 'John Doe'
+      },
+      {
+        id: 4,
+        name: 'Boxing',
+        from: '18:30',
+        to: '19:30',
+        coach: 'John Doe'
+      }
+    ]
+  },
+  {
+    id: 4,
+    day: 'Thursday',
+    class: [
+      {
+        id: 1,
+        name: 'Yoga',
+        from: '8:30',
+        to: '9:15',
+        coach: 'John Doe'
+      },
+      {
+        id: 2,
+        name: 'Crossfit',
+        from: '11:00',
+        to: '11:50',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 3,
+        name: 'Yoga',
+        from: '16:00',
+        to: '16:45',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 4,
+        name: 'Crossfit',
+        from: '17:30',
+        to: '18:20',
+        coach: 'John Doe'
+      },
+      {
+        id: 5,
+        name: 'Boxing',
+        from: '19:00',
+        to: '20:00',
+        coach: 'John Doe'
+      }
+    ]
+  },
+  {
+    id: 5,
+    day: 'Friday',
+    class: [
+      {
+        id: 1,
+        name: 'Yoga',
+        from: '9:30',
+        to: '10:15',
+        coach: 'John Doe'
+      },
+      {
+        id: 2,
+        name: 'Crossfit',
+        from: '14:00',
+        to: '14:50',
+        coach: 'John Doe'
+      },
+      {
+        id: 3,
+        name: 'Crossfit',
+        from: '17:00',
+        to: '17:50',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 4,
+        name: 'Boxing',
+        from: '19:15',
+        to: '20:15',
+        coach: 'John Doe'
+      }
+    ]
+  },
+  {
+    id: 6,
+    day: 'Saturday',
+    class: [
+      {
+        id: 1,
+        name: 'Yoga',
+        from: '10:00',
+        to: '10:45',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 2,
+        name: 'Crossfit',
+        from: '13:00',
+        to: '13:50',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 3,
+        name: 'Yoga',
+        from: '15:00',
+        to: '15:45',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 4,
+        name: 'Boxing',
+        from: '17:45',
+        to: '18:45',
+        coach: 'John Doe'
+      }
+    ]
+  },
+  {
+    id: 7,
+    day: 'Sunday',
+    class: [
+      {
+        id: 1,
+        name: 'Yoga',
+        from: '9:00',
+        to: '9:45',
+        coach: 'John Doe'
+      },
+      {
+        id: 2,
+        name: 'Crossfit',
+        from: '11:00',
+        to: '11:50',
+        coach: 'Jane Doe'
+      },
+      {
+        id: 3,
+        name: 'Crossfit',
+        from: '15:00',
+        to: '15:50',
+        coach: 'John Doe'
+      },
+      {
+        id: 4,
+        name: 'Boxing',
+        from: '18:00',
+        to: '19:00',
+        coach: 'John Doe'
+      }
+    ]
+  }
+]
+
+export default timetableDB
