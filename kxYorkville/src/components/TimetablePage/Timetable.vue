b6e32e352f8dbebc29e03cccfd3f118bb6bf9375
TLNRB <nortol01@easv365.dk>
2023-10-01 12:40:33 +0200

    timetable adjustments

--- kxYorkville/src/components/TimetablePage/Timetable.vue
+++ kxYorkville/src/components/TimetablePage/Timetable.vue
@@ -14,7 +14,7 @@ const { singleClass } = defineProps(['singleClass'])
         <p class="text-[.875rem]">with {{ singleClass.coach }}</p>
       </div>
       <div class="font-oswald text-[.875rem]">
-        <p class="text-bgColorLightest">{{ singleClass.from }}-{{ singleClass.to }}</p>
+        <p class="text-bgColorLightest">{{ singleClass.from }} - {{ singleClass.to }}</p>
         <p class="text-primaryColor font-[500]">{{ singleClass.size }} left</p>
       </div>
     </div>
@@ -24,23 +24,15 @@ const { singleClass } = defineProps(['singleClass'])
       :style="`background-image: url('${singleClass.img}')`"
     >
       <div
-        class="absolute left-0 top-0 right-0 bottom-0 brightness-[80%] backdrop-blur-[2px] z-[0] rounded-b-[10px] drop-shadow-xl"
+        class="absolute left-0 top-0 right-0 bottom-0 brightness-[85%] backdrop-blur-[2px] z-[0] rounded-b-[10px] drop-shadow-xl"
       ></div>
       <button class="font-oswald flex flex-col w-fit text-[1rem] relative group drop-shadow-lg">
+        <span class="py-[.25rem] px-[1rem] bg-bgColorDark z-[1]">Reserve</span>
         <span
-          class="py-[.25rem] px-[1rem] bg-bgColorDark z-[1] md:py-[.375rem] md:px-[1.125rem] md:text-[1.125rem]"
-          >Reserve</span
-        >
-        <span
-          class="absolute top-0 left-0 right-0 bottom-0 text-transparent bg-transparent border-[1px] border-primaryColor scale-[100%] group-hover:scale-y-[125%] group-hover:scale-x-[110%] ease-in duration-[.15s]"
+          class="absolute top-0 left-0 right-0 bottom-0 text-transparent bg-transparent border-[1px] border-primaryColor group-hover:scale-y-[125%] group-hover:scale-x-[110%] ease-in duration-[.15s]"
           >Reserve</span
         >
       </button>
-      <!-- <button
-        class="font-oswald flex flex-col text-[1rem] font-[500] py-[.25rem] px-[1rem] bg-bgColorDark ease-in duration-[.15s] hover:scale-[105%] z-[1] md:py-[.375rem] md:px-[1.125rem] md:text-[1.125rem]"
-      >
-        <span class="">Cancel</span>
-      </button> -->
     </div>
   </section>
 </template>
