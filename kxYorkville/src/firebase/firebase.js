bdbb8d58bc10d4a2d3d8c223c297aa164436b3b2
TLNRB <nortol01@easv365.dk>
2023-10-04 11:05:30 +0200

    firebase config separated and added to git ignore

--- kxYorkville/src/firebase/firebase.js
+++ kxYorkville/src/firebase/firebase.js
@@ -1,16 +1,10 @@
+//Import firebase config
+import firebaseConfig from './firebase-config.js'
 import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: 'AIzaSyDoP1XlO2qvguMHohjOEMczLsB4SOXdpQ0',
-  authDomain: 'gym-kxyorkville.firebaseapp.com',
-  projectId: 'gym-kxyorkville',
-  storageBucket: 'gym-kxyorkville.appspot.com',
-  messagingSenderId: '856204947945',
-  appId: '1:856204947945:web:4936049a2db85f294f1b78'
-}
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig)
