c4e0484e548ac6e973192e70319708e95fe13c1a
TLNRB <nortol01@easv365.dk>
2023-10-06 13:55:31 +0200

    admin title and section blocks

---
+++ kxYorkville/src/components/Admin/General.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <main
+    class="mt-[4rem] pt-[4rem] pb-[2rem] px-[1rem] xs:pt-[5rem] sm:pt-[9rem] md:px-[2rem] lg:pt-[11rem] xxl:pt-[13rem] xxxl:pt-[15rem] xxxxl:px-[4rem] xxxxl:pt-[16rem] xxxxl:mt-[6rem]"
+  ></main>
+</template>
+
+<style scoped></style>
--- kxYorkville/src/views/AdminView.vue
+++ kxYorkville/src/views/AdminView.vue
@@ -1,3 +1,73 @@
-<script setup></script>
-<template></template>
+<script setup>
+import { ref } from 'vue'
+/*----- Importing components -----*/
+import General from '../components/Admin/General.vue'
+
+/*----- Admin section display -----*/
+const generalActive = ref(true)
+const classesActive = ref(false)
+const coachesActive = ref(false)
+const active = 'text-primaryColor border-primaryColor'
+const inactive = 'text-textGray border-textGray'
+
+// Toggle general section
+const toggleGeneral = () => {
+  generalActive.value = true
+  classesActive.value = false
+  coachesActive.value = false
+}
+
+// Toggle classes section
+const toggleClasses = () => {
+  generalActive.value = false
+  classesActive.value = true
+  coachesActive.value = false
+}
+
+// Toggle coaches section
+const toggleCoaches = () => {
+  generalActive.value = false
+  classesActive.value = false
+  coachesActive.value = true
+}
+</script>
+
+<template>
+  <main
+    class="mt-[4rem] pt-[4rem] pb-[2rem] px-[1rem] xs:pt-[5rem] sm:pt-[9rem] md:px-[2rem] lg:pt-[11rem] xxl:pt-[13rem] xxxl:pt-[15rem] xxxxl:px-[4rem] xxxxl:pt-[16rem] xxxxl:mt-[6rem]"
+  >
+    <h1
+      class="absolute top-[4rem] leading-none tracking-tighter text-[4.5rem] translate-x-[-1.125rem] font-semibold text-textDark xs:text-[5.5rem] xs:top-[4rem] sm:text-[9rem] sm:translate-x-[-1.25rem] sm:top-[5rem] md:left-0 md:sm:translate-x-0 lg:text-[12rem] xxl:text-[14rem] xxxl:text-[16rem] xxxxl:text-[20rem]"
+    >
+      admin
+    </h1>
+    <section
+      class="flex items-center justify-center gap-[1.5rem] mt-[2rem] font-oswald xs:justify-start lg:mt-[3rem] lg:text-[1.25rem]"
+    >
+      <button
+        class="pb-[.125rem] border-b-[1px] duration-[.15s] ease-in-out z-[5] cursor-pointer"
+        :class="generalActive ? active : inactive"
+        @click="toggleGeneral"
+      >
+        General
+      </button>
+      <button
+        class="pb-[.125rem] border-b-[1px] duration-[.15s] ease-in-out z-[5] cursor-pointer"
+        :class="classesActive ? active : inactive"
+        @click="toggleClasses"
+      >
+        Classes
+      </button>
+      <button
+        class="pb-[.125rem] border-b-[1px] duration-[.15s] ease-in-out z-[5] cursor-pointer"
+        :class="coachesActive ? active : inactive"
+        @click="toggleCoaches"
+      >
+        Coaches
+      </button>
+    </section>
+    <General />
+  </main>
+</template>
+
 <style scoped></style>
