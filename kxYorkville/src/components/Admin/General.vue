f1e55422b0d09b70f45034eae47a8efd4bb3167a
TLNRB <nortol01@easv365.dk>
2023-10-06 18:19:38 +0200

    general info display done

--- kxYorkville/src/components/Admin/General.vue
+++ kxYorkville/src/components/Admin/General.vue
@@ -1,62 +1,151 @@
 <script setup></script>
 
 <template>
-  <section class="mt-[3rem] pb-[2rem] lg:mt-[4rem] xxxxl:mt-[6rem]">
-    <!-- Opening -->
-    <div class="max-w-[750px] md:mx-auto">
-      <h2 class="mb-[1rem] text-[1.25rem] text-textGray font-[600]">Opening Hours</h2>
-      <div
-        class="flex flex-col mb-[1.5rem] gap-[1.5rem] p-[1.25rem] bg-bgNormal border-[1px] border-primaryColor sm:gap-[2rem]"
-      >
-        <!-- Monday-Friday -->
+  <main
+    class="flex flex-wrap justify-center mt-[3rem] gap-[3rem] pb-[2rem] lg:gap-[4rem] lg:mt-[4rem] xxxxl:gap-[6rem] xxxxl:mt-[6rem]"
+  >
+    <!-- Contact -->
+    <section class="w-[100%] sm:w-[450px] md:w-[600px] lg:w-[750px]">
+      <div>
+        <h2 class="mb-[1rem] text-[1.25rem] text-textGray font-[600]">Contact & Version</h2>
         <div
-          class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
+          class="flex flex-col mb-[1.5rem] gap-[1.5rem] p-[1.25rem] bg-bgNormal border-[1px] border-primaryColor sm:gap-[2rem]"
         >
-          <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Monday-Friday</h3>
-          <input
-            type="text"
-            value="07H-22H"
-            class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
-          />
-        </div>
-        <!-- Saturday -->
-        <div
-          class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
-        >
-          <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Saturday</h3>
-          <input
-            type="text"
-            value="08H-22H"
-            class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
-          />
-        </div>
-        <!-- Sunday -->
-        <div
-          class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
-        >
-          <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Sunday</h3>
-          <input
-            type="text"
-            value="08H-20H"
-            class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
-          />
+          <!-- Phone -->
+          <div
+            class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
+          >
+            <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Phone</h3>
+            <p
+              class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
+            >
+              +1 596-322-7824
+            </p>
+          </div>
+          <!-- Email -->
+          <div
+            class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
+          >
+            <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Email</h3>
+            <p
+              class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
+            >
+              INFO@KXYORKVILLE.COM
+            </p>
+          </div>
+          <!-- Address -->
+          <div
+            class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
+          >
+            <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Address</h3>
+            <p
+              class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
+            >
+              263 DAVENPORT RD, TORONTO, ON M5R 1J9, CANADA
+            </p>
+          </div>
+          <!-- Version -->
+          <div
+            class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
+          >
+            <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Version</h3>
+            <p
+              class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
+            >
+              2023 © KX Yorkville
+            </p>
+          </div>
         </div>
       </div>
-    </div>
-    <button
-      type="submit"
-      class="font-oswald flex flex-col w-fit text-[1rem] relative group md:mx-auto"
-    >
-      <span
-        class="font-[500] py-[.25rem] px-[1rem] border-[1px] border-bgColorDark z-[1] ease-in duration-[.15s] delay-[.05s] md:py-[.375rem] md:px-[1.125rem] md:text-[1.125rem]"
-        >Save</span
-      >
-      <span
-        class="font-[500] w-[0px] py-[.25rem] text-transparent bg-bgColorDark border-y-[1px] border-transparent absolute group-hover:w-[100%] group-hover:px-[1.125rem] ease-in duration-[.2s] md:py-[.375rem] md:group-hover:px-[1.125rem] md:text-[1.125rem]"
-        >Save</span
-      >
-    </button>
-  </section>
+      <div class="flex gap-[1rem] md:gap-[1.5rem] md:justify-center">
+        <button class="font-oswald flex flex-col w-fit text-[1rem] relative group">
+          <span
+            class="font-[500] py-[.25rem] px-[1rem] border-[1px] border-bgColorDark z-[1] ease-in duration-[.15s] delay-[.05s] md:py-[.375rem] md:px-[1.125rem] md:text-[1.125rem]"
+            >Edit</span
+          >
+          <span
+            class="font-[500] w-[0px] py-[.25rem] text-transparent bg-bgColorDark border-y-[1px] border-transparent absolute group-hover:w-[100%] group-hover:px-[1.125rem] ease-in duration-[.2s] md:py-[.375rem] md:group-hover:px-[1.125rem] md:text-[1.125rem]"
+            >Edit</span
+          >
+        </button>
+        <button type="submit" class="font-oswald flex flex-col w-fit text-[1rem] relative group">
+          <span
+            class="font-[500] py-[.25rem] px-[1rem] border-[1px] border-bgColorDark z-[1] ease-in duration-[.15s] delay-[.05s] md:py-[.375rem] md:px-[1.125rem] md:text-[1.125rem]"
+            >Save</span
+          >
+          <span
+            class="font-[500] w-[0px] py-[.25rem] text-transparent bg-bgColorDark border-y-[1px] border-transparent absolute group-hover:w-[100%] group-hover:px-[1.125rem] ease-in duration-[.2s] md:py-[.375rem] md:group-hover:px-[1.125rem] md:text-[1.125rem]"
+            >Save</span
+          >
+        </button>
+      </div>
+    </section>
+    <!-- Opening -->
+    <section class="w-[100%] sm:w-[450px] md:w-[600px] lg:w-[750px]">
+      <div>
+        <h2 class="mb-[1rem] text-[1.25rem] text-textGray font-[600]">Opening Hours</h2>
+        <div
+          class="flex flex-col mb-[1.5rem] gap-[1.5rem] p-[1.25rem] bg-bgNormal border-[1px] border-primaryColor sm:gap-[2rem]"
+        >
+          <!-- Monday-Friday -->
+          <div
+            class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
+          >
+            <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Monday-Friday</h3>
+            <p
+              class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
+            >
+              07H-22H
+            </p>
+          </div>
+          <!-- Saturday -->
+          <div
+            class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
+          >
+            <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Saturday</h3>
+            <p
+              class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
+            >
+              08H-22H
+            </p>
+          </div>
+          <!-- Sunday -->
+          <div
+            class="flex flex-col gap-[.5rem] sm:flex-row sm:items-center md:flex-col md:items-start lg:flex-row lg:items-center"
+          >
+            <h3 class="text-[.875rem] text-textGray sm:text-[1rem] sm:w-[200px]">Sunday</h3>
+            <p
+              class="w-[100%] bg-bgDark py-[.25rem] px-[.75rem] text-[.875rem] outline-none border-[1px] border-bgColorDark sm:py-[.25rem] sm:px-[.875rem] sm:text-[1rem]"
+            >
+              08H-20H
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="flex gap-[1rem] md:gap-[1.5rem] md:justify-center">
+        <button class="font-oswald flex flex-col w-fit text-[1rem] relative group">
+          <span
+            class="font-[500] py-[.25rem] px-[1rem] border-[1px] border-bgColorDark z-[1] ease-in duration-[.15s] delay-[.05s] md:py-[.375rem] md:px-[1.125rem] md:text-[1.125rem]"
+            >Edit</span
+          >
+          <span
+            class="font-[500] w-[0px] py-[.25rem] text-transparent bg-bgColorDark border-y-[1px] border-transparent absolute group-hover:w-[100%] group-hover:px-[1.125rem] ease-in duration-[.2s] md:py-[.375rem] md:group-hover:px-[1.125rem] md:text-[1.125rem]"
+            >Edit</span
+          >
+        </button>
+        <button type="submit" class="font-oswald flex flex-col w-fit text-[1rem] relative group">
+          <span
+            class="font-[500] py-[.25rem] px-[1rem] border-[1px] border-bgColorDark z-[1] ease-in duration-[.15s] delay-[.05s] md:py-[.375rem] md:px-[1.125rem] md:text-[1.125rem]"
+            >Save</span
+          >
+          <span
+            class="font-[500] w-[0px] py-[.25rem] text-transparent bg-bgColorDark border-y-[1px] border-transparent absolute group-hover:w-[100%] group-hover:px-[1.125rem] ease-in duration-[.2s] md:py-[.375rem] md:group-hover:px-[1.125rem] md:text-[1.125rem]"
+            >Save</span
+          >
+        </button>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style scoped></style>
