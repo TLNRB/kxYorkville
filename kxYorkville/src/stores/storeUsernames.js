66732f90b7d15f002631f652b8ff981a6b3185f0
TLNRB <nortol01@easv365.dk>
2023-10-16 11:32:41 +0200

    store username created

---
+++ kxYorkville/src/stores/storeUsernames.js
@@ -0,0 +1,67 @@
+import { defineStore } from 'pinia'
+import { collection, onSnapshot, doc, addDoc, deleteDoc, updateDoc } from 'firebase/firestore'
+import { getStorage, ref, uploadBytes, getDownloadURL, deleteObject } from 'firebase/storage'
+import { db } from '../firebase/firebase.js'
+
+const usernamesCollectionRef = collection(db, 'usernames')
+
+export const useStoreUsernames = defineStore('storeUsernames', {
+  state: () => {
+    return {
+      usernames: []
+    }
+  },
+
+  actions: {
+    // Get Usernames
+    async getUsernames() {
+      const unsubscribe = onSnapshot(usernamesCollectionRef, (querySnapshot) => {
+        let usernames = []
+        querySnapshot.forEach((doc) => {
+          let username = {
+            id: doc.id,
+            username: doc.data().username
+          }
+          usernames.push(username)
+        })
+        this.usernames = usernames
+      })
+    },
+    // Add Username
+    async addUsername(username) {
+      let condition = false
+      for (let i = 0; i < this.usernames.length; i++) {
+        if (this.usernames[i].username === username) {
+          condition = true
+          return
+        }
+      }
+      if (condition === false) {
+        await addDoc(usernamesCollectionRef, {
+          username: username
+        })
+        return 'Username created'
+      } else {
+        return 'Username already exists'
+      }
+    },
+    // Update Username
+    async updateUsername(username, id) {
+      let condition = false
+      for (let i = 0; i < this.usernames.length; i++) {
+        if (this.usernames[i].username === username) {
+          condition = true
+          return
+        }
+      }
+      if (condition === false) {
+        await updateDoc(doc(usernamesCollectionRef, id), {
+          username: username
+        })
+        return 'Username updated'
+      } else {
+        return 'Username already exists'
+      }
+    }
+  }
+})
