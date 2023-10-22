9b98516fc3d37ad3c5e2e77d751f46bb984cc232
TLNRB <nortol01@easv365.dk>
2023-10-22 13:47:02 +0200

    cloud functions wrote

--- kxYorkville/functions/index.js
+++ kxYorkville/functions/index.js
@@ -1,19 +1,69 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const { onRequest } = require('firebase-functions/v2/https')
+// The Cloud Functions for Firebase SDK to set up triggers and logging.
+const { onSchedule } = require('firebase-functions/v2/scheduler')
+const logger = require('firebase-functions/logger')
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const admin = require('firebase-admin')
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+const serviceAccount = require('./service-account-key.json')
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+})
+
+exports.bookingsCleanup = onSchedule('every day 00:00', async (event) => {
+  /*===== Delete bookings made on previus day =====*/
+  const currentDateTime = new Date()
+  console.log('currentDateTime', currentDateTime)
+  // Calculate the datetime of the previous date
+  currentDateTime.setDate(currentDateTime.getDate() - 1)
+
+  const previusDate = currentDateTime.getDate()
+  console.log('previusDate', previusDate)
+
+  // Reference to your Firestore collection
+  const bookingsRef = admin.firestore().collection('bookings')
+
+  // Query bookings made on the previous day
+  const querySnapshot = await bookingsRef.where('day', '==', previusDate).get()
+
+  // Delete the bookings
+  const batch = admin.firestore().batch()
+  querySnapshot.forEach((doc) => {
+    console.log('preparing to delete', doc.id)
+    batch.delete(doc.ref)
+  })
+  console.log('batch length', batch.length ? batch.length : 'batch length is not a property')
+
+  // Commit the batch
+  await batch.commit()
+  console.log('deleted bookings', doc.id)
+
+  /*===== Set the reserved number of classes back to 0 on each class on previus day =====*/
+  // Calculate the datetime of the previous day
+  const previousDay = currentDateTime.getDay()
+  console.log('previousDay', previousDay)
+  // Reference to your Firestore collection
+  const timetableRef = admin.firestore().collection('timetable')
+  // Get the previous day doc of the timetable collection
+  const dayDoc = await timetableRef
+    .doc('ZB5rtoe3MxG2BZNflqbZ')
+    .collection('days')
+    .doc(previousDay.toString())
+    .get()
+  console.log('dayDoc', dayDoc)
+
+  // Converting to an object
+  const day = { ...dayDoc.data() }
+  console.log('day', day)
+
+  day.classes.forEach((classItem) => {
+    classItem.reserved = 0
+  })
+
+  // Update the previous day doc
+  await dayDoc.ref.update(day)
+  console.log('dayDoc updated', dayDoc.id)
+
+  return null
+})
