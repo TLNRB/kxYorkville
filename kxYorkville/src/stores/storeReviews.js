893f968a938612548c593117b86b1ef8d34c0b37
TLNRB <nortol01@easv365.dk>
2023-10-22 18:05:59 +0200

    reviews store created

---
+++ kxYorkville/src/stores/storeReviews.js
@@ -0,0 +1,46 @@
+import { defineStore } from 'pinia'
+import { collection, onSnapshot, doc, addDoc, deleteDoc } from 'firebase/firestore'
+import { db } from '../firebase/firebase.js'
+
+const reviewsCollectionRef = collection(db, 'reviews')
+
+export const useStoreReviews = defineStore('storeReviews', {
+  state: () => {
+    return {
+      reviews: []
+    }
+  },
+
+  actions: {
+    // Get Reviews
+    async getReviews() {
+      const unsubscribe = onSnapshot(reviewsCollectionRef, (querySnapshot) => {
+        let reviews = []
+        querySnapshot.forEach((doc) => {
+          let review = {
+            id: doc.id,
+            userID: doc.data().userID,
+            username: doc.data().username,
+            review: doc.data().review,
+            date: doc.data().date
+          }
+          reviews.push(review)
+        })
+        this.reviews = reviews
+      })
+    },
+    // Add Review
+    async addReview(reviewContent) {
+      await addDoc(reviewsCollectionRef, {
+        userID: reviewContent.userID,
+        username: reviewContent.username,
+        review: reviewContent.review,
+        date: reviewContent.date
+      })
+    },
+    // Delete Review
+    async deleteReview(id) {
+      await deleteDoc(doc(reviewsCollectionRef, id))
+    }
+  }
+})
